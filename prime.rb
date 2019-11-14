df38f29bf2f686b205d7d035a76d3b3b16f7de29
Nick Solonyy <nicksolony@gmail.com>
2019-11-14 02:45:47 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,15 @@
 def prime?(int)
-
+n=2
 if int <= 1  
   false
 else
-  numbers = (2..int-1).to_a
-  numbers.each |n| do  
-
-   if int%n == 0
+  #numbers = (2..int-1).to_a
+  #numbers.each |n| do  
+  while int-1>n do
+    if int%n !== 0
+      n+=1
   
-  n+=1
+    
   end
   
 end
